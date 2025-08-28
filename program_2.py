2b0f646682ceff731ccdb2847e31e46cd7ad95f3
notGriffin <104639986+scuyul@users.noreply.github.com>
2025-08-27 21:31:18 -0500

    Update program_2.py
    added print statemints

--- program_2.py
+++ program_2.py
@@ -1,6 +1,19 @@
-def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
-    
+from ftplib import print_line
 
-proverbs_22_29()
+
+def hello_world():
+    #Greeter  By Griffin Corniea (scuyul) 8/27/2025
+    print_line('“Do you see a man skillful in his work? ')
+    print_line('He will stand before kings; ')
+    print_line('he will not stand before obscure men.” ')
+    print_line('    (Proverbs 22:29) ')
+
+hello_world()
+
+
+
+# output:
+# “Do you see a man skillful in his work?
+# He will stand before kings;
+# he will not stand before obscure men.”
+#     (Proverbs 22:29) 
